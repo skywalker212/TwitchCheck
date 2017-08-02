d2710a67170772a6d1c740448a10ec105d1f21bc
Akash Gajjar <akashgajjar8@gmail.com>
2017-08-02 17:10:49 +0530

    implemented the dynamic search function

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ This a web page that shows whether a channel is currently streaming on Twitch TV
 
 ## I have to fulfil these [User Stories](https://en.wikipedia.org/wiki/User_story)  
 
-- I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel.  
-- I can see whether Free Code Camp is currently streaming on Twitch.tv.  
+- I can click the status output and be sent directly to that channel's Twitch.tv channel.  
+- I can see whether a channel is currently streaming on Twitch.tv.  
 - I can see if a Twitch user is currently streaming, I can see additional details about what they are streaming.  
 - I will see a placeholder notification if a streamer has closed their Twitch account (or the account never existed).  
 
--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+console.log("ready!");
 
+var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "brunofinX", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404"];
+var maincontent = "";
+for (var i = 0; i < streamers.length; i++) {
+  maincontent += "<div class='stms'><a class='link' href='https://www.twitch.tv/" +streamers[i] +"' target='_blank'>" +streamers[i] +"</a></div>";
+}
+
+$(".mainresults").html(maincontent);
+
+$(".text-content").keyup(function () {
+  maincontent="";
+  console.log("pressing the key");
+  var value = $(this).val();
+  for (var i = 0; i < streamers.length; i++) {
+    if(streamers[i].toLowerCase().indexOf(value)!=-1)
+    maincontent += "<div class='stms'><a class='link' href='https://www.twitch.tv/" +streamers[i] +"' target='_blank'>" +streamers[i] +"</a></div>";
+  }
+  $(".mainresults").html(maincontent);
+});
+
+$(".src-btn").on("click", function(event) {
+  event.preventDefault();
+  console.log("search button was clicked");
+  var query = $(".text-content").val();
+  console.log(query);
+});
