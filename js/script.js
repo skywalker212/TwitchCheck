d658df040b951a4037a3ae4665bc5e2bfc00bf07
Akash Gajjar <akashgajjar8@gmail.com>
2017-08-03 16:34:08 +0530

    added ajax request code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,52 @@
-console.log("ready!");
+var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "brunofinX", "habathcx", "RobotCaleb", "noobs2ninjas", "MedryBW", "comster404"];
 
-var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "brunofinX", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404"];
 var maincontent = "";
+var avail;
+var status;
+var imageLink;
+var description;
+
 for (var i = 0; i < streamers.length; i++) {
-  maincontent += "<div class='stms'><a class='link' href='https://www.twitch.tv/" +streamers[i] +"' target='_blank'>" +streamers[i] +"</a></div>";
+
+  $.ajax({
+
+    url: "https://wind-bow.glitch.me/twitch-api/channels/" + streamers[i],
+    dataType: 'json',
+    type: 'GET',
+    success: function(reply) {
+      if (reply.status == 404) {
+        avail = 404;
+      } else {
+        avail = "Active Account";
+        $.ajax({
+          url: "https://wind-bow.glitch.me/twitch-api/streams/" + streamers[i],
+          dataType: 'json',
+          type: 'GET',
+          success: function(data) {
+            if (data.stream == null) {
+              status = "offline";
+            } else {
+              imageLink = data.channel.logo;
+              description = data.channel.status;
+
+            }
+          }
+        });
+      }
+    }
+  });
+  maincontent += "<div class='stms'><a class='link' href='https://www.twitch.tv/" + streamers[i] + "' target='_blank'>" + streamers[i] + "</a></div>";
 }
 
 $(".mainresults").html(maincontent);
 
-$(".text-content").keyup(function () {
-  maincontent="";
-  console.log("pressing the key");
+$(".text-content").keyup(function() {
+  maincontent = "";
   var value = $(this).val();
   for (var i = 0; i < streamers.length; i++) {
-    if(streamers[i].toLowerCase().indexOf(value)!=-1)
-    maincontent += "<div class='stms'><a class='link' href='https://www.twitch.tv/" +streamers[i] +"' target='_blank'>" +streamers[i] +"</a></div>";
+    if (streamers[i].toLowerCase().indexOf(value) != -1) {
+      maincontent += "<div class='stms'><a class='link' href='https://www.twitch.tv/" + streamers[i] + "' target='_blank'>" + streamers[i] + "</a></div>";
+    }
   }
   $(".mainresults").html(maincontent);
 });
