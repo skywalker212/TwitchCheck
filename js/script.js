f6b6d4287732eb117e4e4380bd7b611720731289
Akash Gajjar <akashgajjar8@gmail.com>
2017-08-04 03:19:30 +0530

    finished implementing main features

--- js/script.js
+++ js/script.js
@@ -1,59 +1,68 @@
-var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "brunofinX", "habathcx", "RobotCaleb", "noobs2ninjas", "MedryBW", "comster404"];
+var streamers = ["OgamingSC2", "cretetion", "freecodecamp", "storbeck", "brunofinX", "habathcx", "RobotCaleb", "noobs2ninjas", "MedryBW", "comster404"];
 
-var maincontent = "";
-var avail;
-var status;
-var imageLink;
-var description;
 
-for (var i = 0; i < streamers.length; i++) {
+function getStreamers() {
+  streamers.forEach(function functionName(strm) {
+    var status;
+    var logo;
+    var onoff;
+    var html;
+    $.getJSON("https://wind-bow.glitch.me/twitch-api/channels/" + strm + "?callback=?", function(data) {
+      status = data.status;
+      logo = data.logo;
+      if (status == 404) {
+        status = "Channel has been Closed";
+        onoff = "Account Closed";
+      } else if (status == null) status = "Details not Available";
+      $.getJSON("https://wind-bow.glitch.me/twitch-api/streams/" + strm + "?callback=?", function(data) {
+        if (onoff == "Account Closed") onoff = "N/A"
+        if (data.stream == null) onoff = "offline";
+        else onoff = "online";
+        link = "https://www.twitch.tv/" + strm;
+        if(status.indexOf("Closed") != -1) status = "N/A";
+        else if(onoff=="offline") status = "offline";
+        html = "<div class = 'row result " +
+          onoff + " " + strm + "'><img class='col-md-4 image img-circle img-responsive' src='" +
+          logo + "' alt='Image not Available' ><a class = 'col-md-4 link' target = '_blank' href = '" +
+          link + "'>" + strm + "</a><span class='col-md-4 description hidden-xs'>" +
+          status + "</span></div>";
+        $(".mainresults").append(html);
+      });
 
-  $.ajax({
-
-    url: "https://wind-bow.glitch.me/twitch-api/channels/" + streamers[i],
-    dataType: 'json',
-    type: 'GET',
-    success: function(reply) {
-      if (reply.status == 404) {
-        avail = 404;
-      } else {
-        avail = "Active Account";
-        $.ajax({
-          url: "https://wind-bow.glitch.me/twitch-api/streams/" + streamers[i],
-          dataType: 'json',
-          type: 'GET',
-          success: function(data) {
-            if (data.stream == null) {
-              status = "offline";
-            } else {
-              imageLink = data.channel.logo;
-              description = data.channel.status;
-
-            }
-          }
-        });
-      }
-    }
+    });
   });
-  maincontent += "<div class='stms'><a class='link' href='https://www.twitch.tv/" + streamers[i] + "' target='_blank'>" + streamers[i] + "</a></div>";
 }
 
-$(".mainresults").html(maincontent);
 
-$(".text-content").keyup(function() {
-  maincontent = "";
-  var value = $(this).val();
-  for (var i = 0; i < streamers.length; i++) {
-    if (streamers[i].toLowerCase().indexOf(value) != -1) {
-      maincontent += "<div class='stms'><a class='link' href='https://www.twitch.tv/" + streamers[i] + "' target='_blank'>" + streamers[i] + "</a></div>";
-    }
+getStreamers();
+
+
+$(".selector").click(function() {
+  $(".selector").removeClass("active");
+  $(this).addClass("active");
+  var status = $(this).attr('id');
+  if (status === "all") {
+    $(".online, .offline").removeClass("hidden");
+  } else if (status === "online") {
+    $(".online").removeClass("hidden");
+    $(".offline").addClass("hidden");
+  } else {
+    $(".offline").removeClass("hidden");
+    $(".online").addClass("hidden");
   }
-  $(".mainresults").html(maincontent);
 });
 
-$(".src-btn").on("click", function(event) {
-  event.preventDefault();
-  console.log("search button was clicked");
-  var query = $(".text-content").val();
-  console.log(query);
+$(".selector").hover(function () {
+  $(".selector").removeClass("active");
+  $(this).addClass("active");
+  var status = $(this).attr('id');
+  if (status === "all") {
+    $(".online, .offline").removeClass("hidden");
+  } else if (status === "online") {
+    $(".online").removeClass("hidden");
+    $(".offline").addClass("hidden");
+  } else {
+    $(".offline").removeClass("hidden");
+    $(".online").addClass("hidden");
+  }
 });
